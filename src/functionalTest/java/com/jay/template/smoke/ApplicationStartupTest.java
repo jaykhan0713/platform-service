aca71680e09c29e43d444478f0add39861eded45
Jay Khan <jaykhan0713@gmail.com>
2026-03-12 17:54:26 -0700

    Updated functional test annotations

--- src/functionalTest/java/com/jay/template/smoke/ApplicationStartupTest.java
+++ src/functionalTest/java/com/jay/template/smoke/ApplicationStartupTest.java
@@ -4,12 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 
 import com.jay.template.common.FunctionalTestBase;
-import com.jay.template.common.SpringBootTestShared;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
-@SpringBootTestShared
 class ApplicationStartupTest extends FunctionalTestBase {
 
     private final ApplicationContext appContext;
